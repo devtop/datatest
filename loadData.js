c2e7dc2109a0202f51e88ae8ed2b27bbd279bac7
Tobias Ranft <tora@judithandresen.com>
2022-09-19 19:58:41 +0200

    Restructure to fetch

--- loadData.js
+++ loadData.js
@@ -1,4 +1,8 @@
 console.log("Loaded loadData.js");
 
-const jsonData= require('./data.json');
-console.log(jsonData);
+// const jsonData= require('./data.json');
+// console.log(jsonData);
+
+fetch('./data.json')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
